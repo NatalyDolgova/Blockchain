3049f5123ddd64ab3a1acf4abe9fe0429d1d1bc9
NatalyDolgova <40904401+NatalyDolgova@users.noreply.github.com>
2022-10-09 14:22:32 +0200

    First commit

---
+++ key/keys.py
@@ -0,0 +1,10 @@
+import random
+
+# Задание 1. Количество ключей
+s = input("number bits = ")
+seq = int(s)
+keys = 2**seq
+#a = random.random()
+print ("Количество ключей,", s,"-битовой последовательности = ",keys)
+
+# Задание 2. Генерация ключа
